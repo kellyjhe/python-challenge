3c0bfd161e5a31db9f0e5a48c5bf433fd843ee34
Kelly He <kellyhe408@gmail.com>
2024-08-22 18:20:24 -0700

    Adding finished homework

---
+++ PyBank/main.py
@@ -0,0 +1,59 @@
+# Dependencies
+import os
+import csv
+
+# Set path for CSV
+csvpath = os.path.join('..', 'python-challenge/PyBank/Resources', 'budget_data.csv')
+
+# Set initial variables
+unique_months = set()
+profit_added = 0
+profit_amt = 0
+loss_amt = 0
+
+# Open CSV to read
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    
+    # Skip header row
+    header = next(csvreader)
+
+    # Set loop through CSV
+    for row in csvreader:
+        
+        # Find total months
+        # Code help from Xpert
+        date = row[0]
+        month = date.split(',')[0]
+        unique_months.add(month)
+
+        # Find total profit
+        profit_loss = int(row[1])
+        profit_added += profit_loss
+
+
+        # Use conditional to find greatest increase and decrease amounts
+        if profit_loss > profit_amt:
+            profit_amt = profit_loss
+            greatest_date = row[0]
+
+        if profit_loss < loss_amt:
+            loss_amt = profit_loss
+            least_date = row[0]
+            
+# Count unique values in total months and convert to integer
+total_months = int(len(unique_months))
+
+total_profit = round(profit_added, 1)
+
+# Calculate average change
+avg_change = round((profit_added / total_months), 2)
+
+# Print
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_profit}")
+print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {greatest_date} (${profit_amt})")
+print(f"Greatest Decrease in Profits: {least_date} (${loss_amt})")
---
+++ PyPoll/main.py
@@ -0,0 +1,76 @@
+# Dependencies
+import os
+import csv
+
+# Set file path
+csvpath = os.path.join('..', 'python-challenge/PyPoll/Resources', 'election_data.csv')
+
+# Set variables
+unique_votes = set()
+names = []
+candidate_votes_list = []
+
+# Open CSV file in both reader and dictionary
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    dictreader = csv.DictReader(csvfile)
+
+    # Skip header row
+    header = next(csvreader)
+
+    # Dictionary
+    candidate_votes = {}
+
+    # Set loop
+    for row in csvreader:
+        # Count amount of votes
+        # Code help from Xpert
+        vote_count = row[0]
+        vote = vote_count.split(',')[0]
+        unique_votes.add(vote)
+
+        # Create list of candidates
+        name = row[2]
+        if name not in names:
+            names.append(name)
+        
+        # Find candidate votes
+        # Code help from Xpert
+        candidate = row[2]
+
+        if candidate in candidate_votes:
+                candidate_votes[candidate] += 1
+        else:
+             candidate_votes[candidate] = 1
+
+    # Create list from dictionary to pull from
+    candidate_votes_list = list(candidate_votes.items())
+
+    # Find percentages
+    candidate1_votes = int(candidate_votes_list[0][1])
+    candidate2_votes = int(candidate_votes_list[1][1])
+    candidate3_votes = int(candidate_votes_list[2][1])
+
+    # Create a tuple that compares greatest value of votes in the Candidate Vote list
+    # Code from Xpert
+    winner = max(candidate_votes_list, key=lambda x: x[1])
+
+# Count amount of total votes       
+total_votes = int(len(unique_votes))
+
+# Calculate percentages
+candidate1_percentage = round(((candidate1_votes / total_votes) * 100), 3)
+candidate2_percentage = round(((candidate2_votes / total_votes) * 100), 3)
+candidate3_percentage = round(((candidate3_votes / total_votes) * 100), 3)
+
+# Print
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+print(f"{names[0]}: {candidate1_percentage}% ({candidate1_votes})")
+print(f"{names[1]}: {candidate2_percentage}% ({candidate2_votes})")
+print(f"{names[2]}: {candidate3_percentage}% ({candidate3_votes})")
+print("-------------------------")
+print(f"Winner: {winner[0]}")
+print("-------------------------")
